ee4f8a558547bc3e854a1be106d2fc0ecbed5620
Bram Heetkamp <bramheetkamp@hotmail.nl>
2024-10-29 19:44:19 +0100

    Initial Commit

---
+++ Dare/DareApp.swift
@@ -0,0 +1,17 @@
+//
+//  DareApp.swift
+//  Dare
+//
+//  Created by Bram Heetkamp on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DareTests/DareTests.swift
@@ -0,0 +1,17 @@
+//
+//  DareTests.swift
+//  DareTests
+//
+//  Created by Bram Heetkamp on 29/10/2024.
+//
+
+import Testing
+@testable import Dare
+
+struct DareTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
